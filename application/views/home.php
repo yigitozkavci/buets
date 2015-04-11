4ab8ab6a2df0d80b36109e167353b5ba20d11274
yigitozkavci <yigitozkavci8@gmail.com>
2015-04-11 23:07:28 +0300

    Front-End Changes
    Skeleton design is implemented, initial assets, framework and images
    are created.

---
+++ application/views/home.php
@@ -0,0 +1,104 @@
+<section id="main">
+    <div class="hidden-xs">
+    	<!-- Navigation -->
+        <nav class="navbar navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="col-xs-12">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="<?php echo base_url(); ?>">
+                            <img src="http://placehold.it/150x50&text=Logo" alt="">
+                        </a>
+                        <div class="clearfix"></div>
+                    </div>
+                    <!-- /.navbar-collapse -->
+                </div>
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Page Content -->
+        <div class="container">
+        
+            <!-- Heading Row -->
+            <div class="row">
+                <div class="col-xs-6">
+                    <img class="img-responsive" src="http://placehold.it/900x700" alt="">
+                </div>
+                <!-- /.col-md-8 -->
+                <div class="col-xs-6">
+                    <h1>Business Name or Tagline</h1>
+                    <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+                    <a class="btn btn-primary btn-lg" href="#">Call to Action!</a>
+                </div>
+                <!-- /.col-md-4 -->
+            </div>
+            <!-- /.row -->
+
+            <hr>
+
+            <!-- Call to Action Well -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="well text-center">
+                        This is a well that is a great spot for a business tagline or phone number for easy access!
+                    </div>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+
+            <!-- Content Row -->
+            <div class="row">
+                <div class="col-md-4">
+                    <h2>Heading 1</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+                    <a class="btn btn-default" href="#">More Info</a>
+                </div>
+                <!-- /.col-md-4 -->
+                <div class="col-md-4">
+                    <h2>Heading 2</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+                    <a class="btn btn-default" href="#">More Info</a>
+                </div>
+                <!-- /.col-md-4 -->
+                <div class="col-md-4">
+                    <h2>Heading 3</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+                    <a class="btn btn-default" href="#">More Info</a>
+                </div>
+                <!-- /.col-md-4 -->
+            </div>
+            <!-- /.row -->
+
+            <!-- Footer -->
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>Copyright &copy; Your Website 2014</p>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+        <!-- /.container -->
+    </div>
+</section>
+<div class="visible-xs">
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">Welcome To Our Studio!</div>
+                <div class="intro-heading">It's Nice To Meet You</div>
+                <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
+            </div>
+        </div>
+    </header>
+</div>
